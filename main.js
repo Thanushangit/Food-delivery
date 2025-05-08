1e1aeeee66c21e9e2cc3b389cd4e1ec6a81f7781
Thanushangit <sinnarasathanushan2002@gmail.com>
2025-05-08 23:02:33 +0530

    header part is completed and hero part is pending

---
+++ main.js
@@ -0,0 +1,20 @@
+// thid code for the slider menu
+
+const sliderE1= document.getElementById('slildermenu');
+const menuiconE1=document.getElementById('menuicon');
+const navlinkE1 = document.querySelectorAll('#nav-link')
+
+menuiconE1.addEventListener('click',()=>{
+    sliderE1.classList.toggle('left-[0]')
+    menuiconE1.classList.toggle('ri-close-large-line')
+    
+})
+
+navlinkE1.forEach(link =>{
+    link.addEventListener('click',()=>{
+        sliderE1.classList.toggle('left-[0]')
+        menuiconE1.classList.toggle('ri-close-large-line')
+        
+    })
+})
+
